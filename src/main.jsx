a7e7dc8c72026e62e1e19faef3e389f171771196
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-01-03 12:50:41 +0000

    :sparkles: feat: Import index.css file and Router file

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../src/styles/index.css';
+import Router from './router/Router';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render();
+ReactDOM.createRoot(document.getElementById('root')).render(<Router />);
